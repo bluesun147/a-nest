d3bfb29841cda2fe501caff9b2c068f801a2c21c
HaeChan Kim <bluesun147@naver.com>
2022-05-11 00:04:26 +0900

    hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   await app.listen(3000);
+  console.log(`listening on port ${port}!`);
+
+  // https://docs.nestjs.com/recipes/hot-reload
+  if (module.hot) { // hot reloading 
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
 }
 bootstrap();
